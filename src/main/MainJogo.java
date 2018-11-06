ff086348ca89e90c7c3a6aebce8bce9e54179eed
aluno <aluno@CEPGBESL05009.estacio.acad>
2018-11-06 19:28:37 -0200

    Adicionado: Metodo pegandoEntrada e mostrarAlerta

--- src/main/MainJogo.java
+++ src/main/MainJogo.java
@@ -1,5 +1,13 @@
 package main;
 
-public class MainJogo {
-    
+import view.TelaJogo;
+
+public class MainJogo 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println(TelaJogo.pegandoEntrada());
+        
+    }
+            
 }
--- src/view/TelaJogo.java
+++ src/view/TelaJogo.java
@@ -1,5 +1,35 @@
 package view;
 
+import java.util.Scanner;
+
 public class TelaJogo {
-    
+
+    public static char pegandoEntrada() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(">> Digite uma Letra: ");
+        char letra = sc.nextLine().charAt(0);
+        System.out.println(">> Pengando Entrada do Jogo <<");
+        return letra;
+    }
+
+    //Verficiando Acerto/Erro
+    public void mostrAlerta(boolean acertou) {
+        if (acertou) {
+            System.out.println(">Acertou<");
+            System.out.println("*** ***");
+            System.out.println(" *   * ");
+            System.out.println("**   ** ");
+            System.out.println(" *   * ");
+            System.out.println(" *   * ");
+            System.out.println("*** ***");
+        } else {
+            System.out.println(">Errou<");
+            System.out.println("+++ +++");
+            System.out.println(" +   + ");
+            System.out.println("++   ++ ");
+            System.out.println(" +   + ");
+            System.out.println(" +   + ");
+            System.out.println("+++ +++");
+        }
+    }
 }
