c4f4a5c90455d3a3f904128997a86fea572c2797
alunoti <alunoti@CEPGBESL05005.estacio.acad>
2018-11-07 18:27:30 -0200

    Adicionado: classe Jogador

---
+++ src/core/Jogador.java
@@ -0,0 +1,30 @@
+package core;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author vitor
+ */
+public class Jogador {
+
+    private int numeroJogadas;
+    private ArrayList<Character> listaJogadas;
+
+    public int getNumeroJogadas() {
+        return numeroJogadas;
+    }
+
+    public void setNumeroJogadas(int numeroJogadas) {
+        this.numeroJogadas = numeroJogadas;
+    }
+
+    public ArrayList<Character> getListaJogadas() {
+        return listaJogadas;
+    }
+
+    public void setListaJogadas(ArrayList<Character> listaJogadas) {
+        this.listaJogadas = listaJogadas;
+    }
+    
+}   
