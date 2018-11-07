0dd3c0ebd6a3c44b46117ea80e98a5b23d9a73fc
alunoti <alunoti@CEPGBESL05005.estacio.acad>
2018-11-07 18:51:45 -0200

    Adicionado: inciado o metodo adicionarJogada

---
+++ src/core/Jogo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+import view.TelaJogo;
+
+/**
+ *
+ * @author alunoti
+ */
+public class Jogo {
+    Jogador jogador;
+    public Jogo(){
+        jogador = new Jogador();
+    }
+    
+   public void adicionarJogada(){
+       //jogador.setListaJogadas(TelaJogo.pegarEntrada());
+   }
+    
+}
